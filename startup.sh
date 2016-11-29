171c0fe76420303ce45389d00d6e58bc32678afe
Abhinav Nath Gupta <abhinavnathgupta@Abhinavs-MacBook-Pro.local>
2016-11-29 10:58:15 +0530

    feat(harmonyapp): modified the startup script to include git install command

--- startup.sh
+++ startup.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 sudo apt-get update --fix-missing
+sudo apt-get install git-all
 curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
 sudo apt-get install nodejs
 npm install

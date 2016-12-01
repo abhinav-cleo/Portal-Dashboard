aa042caf37b1b0432f89a8a8ee1b9d6c183adc93
abhinav-cleo <agupta@cleo.com>
2016-12-01 12:02:26 +0530

    deploy script change

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-/opt/google-cloud-sdk/bin/gcloud compute instances delete instance-2 --zone=us-central1-c --quiet
+now=$(/opt/google-cloud-sdk/bin/gcloud compute instances delete instance-2 --zone=us-central1-c --quiet)
+echo "$now"
